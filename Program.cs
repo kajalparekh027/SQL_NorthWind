bc23cf81b95a4e0e30e26e492700a3d7eabc3d02
Q <Q@DESKTOP-PU2U682>
2022-05-30 23:03:25 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.SqlClient;
+
+string ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=False";
+
+SqlConnection Connection = new(ConnectionString);
+Connection.Open();
+
+SqlCommand Command = new("select * from categories", Connection);
+
+var Result = Command.ExecuteReader();
+
+while (Result.Read()) {
+    var CatName = Result.GetValue(1).ToString();
+    var CatDesc = Result.GetValue(2).ToString();
+    System.Console.WriteLine(CatName.PadRight(15) + ": " + CatDesc);
+}
+
+Connection.Close();
+
+Connection.Open();
+Command.CommandText = "insert into categories (categoryname, description) " +
+    " values ('Shoes', 'Nice shoes')";
+
+Command.ExecuteNonQuery();
+Connection.Close();
